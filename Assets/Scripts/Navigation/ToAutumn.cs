97ff7fa66e27f2b633bc2a0af90a74c14372d3f3
Lzkkkuan <141710483+Lzkkuan@users.noreply.github.com>
2024-07-29 17:03:15 +0800

    updated scene transition and navigation

---
+++ Assets/Scripts/Navigation/ToAutumn.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToAutumn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GoToAutumn()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
